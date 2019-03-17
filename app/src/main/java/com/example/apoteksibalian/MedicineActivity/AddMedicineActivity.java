2fee993840dd8deb22ad42cca8f2fd71dd079270
PrabaBa <prabahridayami97@gmail.com>
2019-03-17 22:21:48 +0800

    medicine

---
+++ app/src/main/java/com/example/apoteksibalian/MedicineActivity/AddMedicineActivity.java
@@ -0,0 +1,15 @@
+package com.example.apoteksibalian.MedicineActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.apoteksibalian.R;
+
+public class AddMedicineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_medicine);
+    }
+}
