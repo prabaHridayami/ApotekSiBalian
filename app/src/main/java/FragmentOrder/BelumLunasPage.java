3b7b3b917754f08bd8e3def56102ee2067b2e140
prabaHridayami <37198788+prabaHridayami@users.noreply.github.com>
2019-04-06 13:33:54 +0800

    detail-order

--- app/src/main/java/FragmentOrder/BelumLunasPage.java
+++ app/src/main/java/FragmentOrder/BelumLunasPage.java
@@ -1,5 +1,6 @@
 package FragmentOrder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,7 +9,9 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.apoteksibalian.DetailorderActivity.DetailOrderActivity;
 import com.example.apoteksibalian.R;
 
 public class BelumLunasPage extends Fragment {
@@ -18,6 +21,17 @@ public class BelumLunasPage extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.pending_page,container,false);
+
+        Button btn_detail = v.findViewById(R.id.btn_detail);
+
+        btn_detail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), DetailOrderActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
         return v;
     }
 }
